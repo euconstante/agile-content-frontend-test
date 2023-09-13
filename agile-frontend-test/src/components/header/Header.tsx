15cb31afdc2624b5ad17a293aacdc60525c8631d
euconstante <constanteguedert@gmail.com>
2023-09-13 18:51:41 -0300

    feat: start header component

--- agile-frontend-test/src/components/header/Header.tsx
+++ agile-frontend-test/src/components/header/Header.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <div className="Footer">
-      <p>TESTE</p>
-    </div>
+    <header className="header">
+      <div className="header__left">
+        <p>
+          <strong>Agile Content </strong>Frontend test
+        </p>
+        {/* TODO: ADD CONDITION TO RENDERING */}
+        {/* <div className="header__search">
+          <input type="text" className="header__input" />
+          <i className="fas fa-search header__searchIcon"></i>
+        </div> */}
+      </div>
+
+      <div className="header__right">
+        <button className="header__btn">Google Search</button>
+        <button className="header__btn">I'm Feeling Lucky</button>
+      </div>
+    </header>
   );
 }
 
